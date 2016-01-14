9032b24e606755b17461d545177d826df60f4e0a
SirBarbier <matthew-7.7@hotmail.com>
2016-01-14 18:51:31 +0000

    added php stuff and some updates

---
+++ testphp.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		
+		<!--my custom css-->
+		<link rel="stylesheet" type="text/css" href="matt.css">
+		</head>
+		
+<body>
+
+<h1 class="jumbotron">My first PHP page</h1>
+<img src="php.jpg" /> <br />
+<?php
+echo "Hello World!<br />";
+echo "5 + 5 is " . (5+5) . "<br />";
+echo "PHP is up and running."?>
+<br /><br /><br />
+<p class="well">Click here to return to the <a href="http://www.matthewbarbier.com/home.html" target="_blank">matthewbarbier.com</a> homepage.</p>
+
+</body>
+</html>
