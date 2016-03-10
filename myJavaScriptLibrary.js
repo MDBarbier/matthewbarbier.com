88c9003bc932a1415ab253c92fd2188d06cb1157
SirBarbier <matthew-7.7@hotmail.com>
2016-03-10 19:35:28 +0000

    added caesar cipher page

--- myJavaScriptLibrary.js
+++ myJavaScriptLibrary.js
@@ -52,3 +52,35 @@ function doesPlayerExist()
 			return "Sorry, your browser does not support Web Storage...";
 		}
 }
+
+function CaesarCipher(str, num) {
+
+    str = str.toLowerCase();
+    var result = '';
+    var charcode = 0;
+
+    for (i = 0; i < str.length; i++) {
+        charcode = (str[i].charCodeAt()) + num;
+        result += String.fromCharCode(charcode);
+    }
+    return result;
+
+}
+
+function encryptMe() {
+	//window.alert("encryption commencing");
+	var string;
+	var offset;
+	var encryptedString;
+	
+	string = $('#phrase').val();
+	offset = parseInt($('#offset').val());
+	encryptedString = CaesarCipher(string, offset);
+	//window.alert("Your encrypted string is: " + encryptedString);
+	$('#output').html('<div class="alert alert-success"><strong>Success!</strong> Your encrypted string is:<br /><br />   ' + encryptedString + ' </div>');
+}
+
+function sayHello () {
+	window.alert("Hello from the JavaScript Library");
+	
+}
