ddaa32c607fe6c57b6eebfa9a9c92898044e781f
SirBarbier <matthew-7.7@hotmail.com>
2017-02-07 20:05:03 +0000

    started login system work

---
+++ includes/psl-config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("HOST", "localhost");
+define("USER", "matt_sec_user");
+define("PASSWORD", "dem1culver1N");
+define("DATABASE", "matt_secure_login");
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", FALSE);
+
+ ?>
